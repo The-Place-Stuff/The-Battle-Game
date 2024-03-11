6ed659c027fa0e390b20b6ae2270ac721fd2eb36
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-03-11 11:39:05 -0500

    Summon items are now `on_use`

--- BP/scripts/mechanics/summon_manager.ts
+++ BP/scripts/mechanics/summon_manager.ts
@@ -1,19 +1,16 @@
 import { world } from '@minecraft/server'
 import { ITEM_TO_SUMMON } from '../constants'
-import { VectorUtils } from '../utils/vector_utils'
 
-world.afterEvents.itemUseOn.subscribe(event => {
+world.afterEvents.itemUse.subscribe(event => {
     const user = event.source
     const dimension = user.dimension
-    const faceLocation = event.faceLocation
     const stack = event.itemStack
 
     if (!ITEM_TO_SUMMON.has(stack.typeId)) {
         return
     }
     const summonId = ITEM_TO_SUMMON.get(stack.typeId)
-    const location = event.block.location
-    const summonManager = dimension.spawnEntity('battle:summon_manager', VectorUtils.add(location, faceLocation))
+    const summonManager = dimension.spawnEntity('battle:summon_manager', user.location)
     const projectile = summonManager.getComponent('minecraft:projectile')
     projectile.owner = user
     summonManager.triggerEvent(`battle:transform_into_${summonId}`)
