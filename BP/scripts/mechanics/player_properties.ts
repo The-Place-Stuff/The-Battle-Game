8534e988634287f520315c2c1302a234ea1533b6
ExDrill <72329798+ExDrill@users.noreply.github.com>
2024-03-15 14:01:02 -0500

    Delete player_properties.ts

--- BP/scripts/mechanics/player_properties.ts
+++
@@ -1,8 +0,0 @@
-import { world } from '@minecraft/server'
-
-world.afterEvents.playerSpawn.subscribe(event => {
-    const player = event.player
-    
-    player.setDynamicProperty('battle:volume', 'none')
-    player.setDynamicProperty('battle:previous_volume', 'none')
-})
